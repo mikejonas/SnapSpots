1dedc21929764f5688087f52c7fa8d3cef4ab83e
Mike Jonas <motion@gmail.com>
2015-10-19 21:26:30 -0700

    Initial Commit

---
+++ SnapSpotsTests/SnapSpotsTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapSpotsTests.swift
+//  SnapSpotsTests
+//
+//  Created by Mike Jonas on 10/19/15.
+//  Copyright © 2015 Mike Jonas. All rights reserved.
+//
+
+import XCTest
+@testable import SnapSpots
+
+class SnapSpotsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
